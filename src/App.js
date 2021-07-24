5f49825db2100f0dd1e09f83bb13e8d921fbb02b
Aakash <tiwaryaakash00@gmail.com>
2021-07-24 18:46:21 +0530

    changes in styling made root.scss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import Board from "./Board";
-
+import "./Styles/root.scss";
 function App() {
   return (
     // for passing the numbers ,expressions or functions from one component to another
     // we use {} braces
 
-    <div>
+    <div className="app">
+      <h1>TIC TAC TOE </h1>
       <Board></Board>
     </div>
   );
--- src/Square.js
+++ src/Square.js
@@ -4,7 +4,11 @@ function Square({ value }) {
   // the curly braces are just destructring
   //the values from props passed
 
-  return <button type="button">{value}</button>;
+  return (
+    <button type="button" className="square">
+      {value}
+    </button>
+  );
 }
 
 export default Square;
